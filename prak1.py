39c1e7c0e9bd3657a0d41bde2df5009eb83acaab
sulthann1 <sulthanaawaliya155@gmail.com>
2025-10-07 14:57:02 +0700

    memasukan persentase nilai siswa

--- prak1.py
+++ prak1.py
@@ -3,4 +3,10 @@ percentage = float((input)"Masukan persentase nilai siswa: ")
 if percentage >= 90 :
     print ("Excelent Perfomance")
 elif percentage = >=80:
-    print
+    print ("Very good perfomance")
+elif percentage >= 70:
+    print ("Good perfomance")
+elif percentage >= 60:
+    print ("Average perfomance")
+else :
+    print ("Needs improvment")
