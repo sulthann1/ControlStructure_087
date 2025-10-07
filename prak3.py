9a3880a84855edc3d3975b33ed2b8dea5d3da56e
sulthann1 <sulthanaawaliya155@gmail.com>
2025-10-07 15:18:30 +0700

    memasukan nilai n untuk mencari bilangan ganjil

--- prak3.py
+++ prak3.py
@@ -1,6 +1,6 @@
-n = int (input ("Masukan nilai n untuk batas deret fibonacci :"))
+n = int (input ("Masukan nilai n untuk batas deret Fibonacci :"))
 
-a,b = 0,1
+a, b = 0,1
 
 print ("deret fibonacci hingga", n, ":")
 while a <= n:
---
+++ prak4.py
@@ -0,0 +1,5 @@
+n = int (input("Masukan nilai n untuk mencari bilangan ganjil : "))
+
+print ("Bilangan ganjil hingga", n, ":")
+for i in range (1, n + 1, 2) :
+    print (i, end=" ")
